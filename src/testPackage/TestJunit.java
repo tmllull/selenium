b5c6a013682d9493f224656c5ff09edfa2e79b5d
allullam <allullam@DES000452.corp.capgemini.com>
2016-11-23 15:35:04 +0100

    More and more test code

---
+++ src/testPackage/TestJunit.java
@@ -0,0 +1,14 @@
+package testPackage;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestJunit {
+	
+   String message = "Hello World";	
+   MessageUtil messageUtil = new MessageUtil(message);
+
+   @Test
+   public void testPrintMessage() {
+      assertEquals(message,messageUtil.printMessage());
+   }
+}
